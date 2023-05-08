b0c99cac54c36d35863eb6725001ee1a21e7cb22
Mancoba <manq2010@gmail.com>
2023-05-08 14:18:29 +0200

    Add css modules

---
+++ pages/posts/first-post.js
@@ -0,0 +1,35 @@
+import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+    <Head>
+        <title>First Post</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+
+      <Image
+        src="/images/profile.jpeg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+        />
+    </Layout>
+  );
+}
