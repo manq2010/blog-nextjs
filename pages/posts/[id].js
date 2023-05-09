ee8cdad0fc6caf0e16a3c068020fd3498d5e3742
Mancoba <manq2010@gmail.com>
2023-05-09 08:13:47 +0200

    Add Date

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+// import Date from '../../components/date';
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
@@ -21,11 +23,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
       <Layout>
+         <Head>
+        <title>{postData.title}</title>
+        </Head>
+
         {postData.title}
         <br />
         {postData.id}
         <br />
         {postData.date}
+        {/* <Date dateString={postData.date} /> */}
       </Layout>
     );
   }
